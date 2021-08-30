026cb5ebae2c717f58549622e2676cf572d42842
Sirish Gambhira <sirishgam001@gmail.com>
2021-08-30 15:35:02 +0530

    Initial Commit

---
+++ core/inference.py
@@ -0,0 +1,91 @@
+import json
+import numpy as np
+import os
+import torch
+import torch.backends.cudnn
+import torch.utils.data
+import cv2
+import utils.binvox_visualization
+import utils.data_loaders
+import utils.data_transforms
+import utils.network_utils
+
+from datetime import datetime as dt
+
+from models.encoder import Encoder
+from models.decoder import Decoder
+from models.refiner import Refiner
+from models.merger import Merger
+
+def inference(cfg,
+             epoch_idx=-1,
+             output_dir=None,
+             test_data_loader=None,
+             test_writer=None,
+             encoder=None,
+             decoder=None,
+             refiner=None,
+             merger=None):
+    # Enable the inbuilt cudnn auto-tuner to find the best algorithm to use
+    torch.backends.cudnn.benchmark = True
+
+    IMG_SIZE = cfg.CONST.IMG_H, cfg.CONST.IMG_W
+    CROP_SIZE = cfg.CONST.CROP_IMG_H, cfg.CONST.CROP_IMG_W
+    test_transforms = utils.data_transforms.Compose([
+        utils.data_transforms.CenterCrop(IMG_SIZE, CROP_SIZE),
+        utils.data_transforms.RandomBackground(cfg.TEST.RANDOM_BG_COLOR_RANGE),
+        utils.data_transforms.Normalize(mean=cfg.DATASET.MEAN, std=cfg.DATASET.STD),
+        utils.data_transforms.ToTensor(),
+    ])
+
+    encoder = Encoder(cfg)
+    decoder = Decoder(cfg)
+    refiner = Refiner(cfg)
+    merger = Merger(cfg)
+
+    if torch.cuda.is_available():
+        encoder = torch.nn.DataParallel(encoder).cuda()
+        decoder = torch.nn.DataParallel(decoder).cuda()
+        refiner = torch.nn.DataParallel(refiner).cuda()
+        merger = torch.nn.DataParallel(merger).cuda()
+
+    print('[INFO] %s Loading weights from %s ...' % (dt.now(), cfg.CONST.WEIGHTS))
+    checkpoint = torch.load(cfg.CONST.WEIGHTS)
+    epoch_idx = checkpoint['epoch_idx']
+    encoder.load_state_dict(checkpoint['encoder_state_dict'])
+    decoder.load_state_dict(checkpoint['decoder_state_dict'])
+
+    encoder.eval()
+    decoder.eval()
+    refiner.eval()
+    merger.eval()
+
+    img1_path = '/ShapeNetRendering/02691156/1a04e3eab45ca15dd86060f189eb133/rendering/00.png'
+    img1_np = cv2.imread(img1_path, cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.
+
+    sample = np.array([img1_np])
+    rendering_images = test_transforms(rendering_images=sample)
+    rendering_images = rendering_images.unsqueeze(0)
+
+    with torch.no_grad():
+        # Get data from data loader
+        rendering_images = utils.network_utils.var_or_cuda(rendering_images)
+
+        # Test the encoder, decoder, refiner and merger
+        image_features = encoder(rendering_images)
+        raw_features, generated_volume = decoder(image_features)
+
+        if cfg.NETWORK.USE_MERGER and epoch_idx >= cfg.TRAIN.EPOCH_START_USE_MERGER:
+            generated_volume = merger(raw_features, generated_volume)
+        else:
+            generated_volume = torch.mean(generated_volume, dim=1)
+
+        if cfg.NETWORK.USE_REFINER and epoch_idx >= cfg.TRAIN.EPOCH_START_USE_REFINER:
+            generated_volume = refiner(generated_volume)
+        
+        generated_volume = generated_volume.squeeze(0)
+
+        img_dir = '/sample_images'
+        gv = generated_volume.cpu().numpy()
+        rendering_views = utils.binvox_visualization.get_volume_views(gv, os.path.join(img_dir),
+                                                                    epoch_idx)
